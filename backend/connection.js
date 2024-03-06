ed5ae771cd3bbe066315e3d36ba66b35e3d65542
Soura700 <108584756+Soura700@users.noreply.github.com>
2024-03-06 10:21:23 +0530

    Change

--- backend/connection.js
+++ backend/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql")
 
 const connection = mysql.createConnection({
     // host: "localhost",
-    host:host.docker.internal,
+    host:"host.docker.internal",
     user:"root",
     password: "root123",
     database:"ecommerce"
